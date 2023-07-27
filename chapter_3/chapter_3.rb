bfd4b64e188eb6de0da0f8a765cf8d42fbd0d7ab
Fh-Ndiritu <fhndiritu@gmail.com>
2023-07-27 15:40:08 +0300

    ARGV example

--- chapter_3/chapter_3.rb
+++ chapter_3/chapter_3.rb
@@ -11,3 +11,5 @@ while i < ARGV[2].to_i
     puts i
     i += 1
 end
+
+#Day3 of #100DaysOfCode and Chapter 3 of '#Ruby Under a Microscope', I came across an interesting thing I have only seen in C, argument vectors. ARGV is a constant and instance of Array class of arguments passed via the command line. 
