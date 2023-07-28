4178611fbd79523adf5f31852b8f0c65fd07b067
Fh-Ndiritu <fhndiritu@gmail.com>
2023-07-28 20:10:21 +0300

    for loop is compiled as each loop

--- chapter_4/chapter_4.rb
+++ chapter_4/chapter_4.rb
@@ -1,4 +1,3 @@
-require 'pp'
 snippet = <<END
     for i in 0..3 do 
         puts i 
@@ -6,3 +5,34 @@ snippet = <<END
 END
 
 puts RubyVM::InstructionSequence.compile(snippet).disasm
+
+
+# Ruby optimizes all program methods including CFUNC or internal C implementations
+# YARV has 11 methods to handle all methods with program related methods being handled by ISEC  - instruction sequence method
+
+
+
+
+
+# Ruby 2.0 used hidden_hashes to implement keyword and positional arguments
+class Hash
+    def has_key?(val)
+        puts "looking for key #{val} in hash"
+        false
+    end
+end
+
+def add_us(a: 1, b: 2)
+    a + b
+end
+
+
+puts add_us(a: 0, b: 7)
+
+
+
+
+
+
+
+
