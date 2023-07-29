b9c6c025a1d0b8838c2fa23d84f34c0d0e01a9ff
Fh-Ndiritu <fhndiritu@gmail.com>
2023-07-29 20:28:41 +0300

    class variables are the same value scoped to all class instances

--- chapter_5_objects_and_classes/chapter_5.rb
+++ chapter_5_objects_and_classes/chapter_5.rb
@@ -1,17 +1,35 @@
 class Car
     @type = 'Electric'
 
+    @@wheel_count = 4
+
     def self.type
         @type
     end
+
+    def self.wheel_count
+        @@wheel_count
+    end
+
 end
 
 class Sedan < Car
     @type = 'Sedan'
+    @@wheel_count = 3
 
     def self.type
         @type
     end
+
+    def self.wheel_count
+        @@wheel_count
+    end
 end
 
 p Sedan.type
+p Car.type
+
+p Sedan.wheel_count
+p Car.wheel_count
+
+
