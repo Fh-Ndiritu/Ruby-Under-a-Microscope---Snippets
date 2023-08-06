af6f6d6056db0e0fa07b3e58a1bd0a0329d0c73d
Fh-Ndiritu <fhndiritu@gmail.com>
2023-08-06 13:05:22 +0300

    ruby speed to retrieve data from an unusually long single bin

---
+++ chapter_7 - hash tables/abnormal_hash_function.rb
@@ -0,0 +1,37 @@
+require 'benchmark'
+
+
+class KeyObject
+    def hash
+        3
+    end
+    
+    def eql?(other)
+        super
+    end
+end
+
+21.times do |exp|
+    target_key = nil
+    size = 2**exp
+    hash = {}
+    (1..size).each do |n|
+        index = KeyObject.new
+        target_key = index if n > size/2 && target_key.nil?
+        hash[index] = rand
+    end
+
+    GC.disable
+
+    Benchmark.bm do |bench|
+        bench.report("retrieving element from hash of #{size} for 10k times ") do
+            10000.times do 
+                val = hash[target_key]
+            end
+        end
+    end
+
+    GC.enable
+
+end
+
