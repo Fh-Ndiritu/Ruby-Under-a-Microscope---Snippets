16588d244de7794c1178084033151b70da80d527
Fh-Ndiritu <fhndiritu@gmail.com>
2023-08-07 22:04:24 +0300

    blocks are slightly slower than a crude while loop

---
+++ chapter_8_blocks/chapter_8.rb
@@ -0,0 +1,26 @@
+require 'benchmark'
+ITERATIONS = 1000000
+Benchmark.bm do |bench|
+    bench.report("iterating from 1 to 10, one million times") do
+    ITERATIONS.times do
+        sum = 0
+        i = 1
+        while i <= 10
+            sum += i
+            i += 1
+        end
+    end
+end
+end
+
+require 'benchmark'
+Benchmark.bm do |bench|
+ bench.report("iterating from 1 to 10, one million times") do
+ ITERATIONS.times do
+ sum = 0
+ (1..10).each do |i|
+ sum += i
+ end
+ end
+ end
+end
