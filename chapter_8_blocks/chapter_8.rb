1dd4515a7c9dd0798893fea16f81331d9425232e
Fh Ndiritu <fhndiritu@gmail.com>
2023-08-20 20:31:18 +0300

    the strangest behavior in Ruby

--- chapter_8_blocks/chapter_8.rb
+++ chapter_8_blocks/chapter_8.rb
@@ -1,26 +1,39 @@
-require 'benchmark'
-ITERATIONS = 1000000
-Benchmark.bm do |bench|
-    bench.report("iterating from 1 to 10, one million times") do
-    ITERATIONS.times do
-        sum = 0
-        i = 1
-        while i <= 10
-            sum += i
-            i += 1
-        end
+# require 'benchmark'
+# ITERATIONS = 1000000
+# Benchmark.bm do |bench|
+#     bench.report("iterating from 1 to 10, one million times") do
+#     ITERATIONS.times do
+#         sum = 0
+#         i = 1
+#         while i <= 10
+#             sum += i
+#             i += 1
+#         end
+#     end
+# end
+# end
+
+# require 'benchmark'
+# Benchmark.bm do |bench|
+#  bench.report("iterating from 1 to 10, one million times") do
+#  ITERATIONS.times do
+#  sum = 0
+#  (1..10).each do |i|
+#  sum += i
+#  end
+#  end
+#  end
+# end
+
+
+def sun_rise
+    str = "The sun rises"
+
+    lambda do |period|
+        puts "#{str} every #{period}"
     end
 end
-end
 
-require 'benchmark'
-Benchmark.bm do |bench|
- bench.report("iterating from 1 to 10, one million times") do
- ITERATIONS.times do
- sum = 0
- (1..10).each do |i|
- sum += i
- end
- end
- end
-end
+function_value = sun_rise
+
+function_value.call('day')
