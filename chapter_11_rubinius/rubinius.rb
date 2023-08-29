d30cf1018e1c668ddeb24b2a9f54478da6cec111
Fh Ndiritu <fhndiritu@gmail.com>
2023-08-29 20:22:50 +0300

    how it works internally

--- chapter_11_rubinius/rubinius.rb
+++ chapter_11_rubinius/rubinius.rb
@@ -1,2 +1,7 @@
 # Rubinius is ruby implemented in Ruby
 # A bit is implemented in C++ as well
+
+#this allows it to use object oriented implementation
+# Rubinius is also more accurate with stacktrace
+
+# Methods like shift in arrays are optimized to avoid creating new arrays or shifting all elements
